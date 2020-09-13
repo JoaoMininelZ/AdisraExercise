f41b7231aaa64a2b50add02420430feadc79d648
João Mininél <51141319+JoaoMininelZ@users.noreply.github.com>
2020-09-13 17:53:23 -0300

    Add files via upload

---
+++ Exercicio 5 - GolombIsSilverman/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GolombIsSilverman {
+    class Silverman {
+        static int silverman(int num) {
+
+            // Padrão.
+            if (num == 1)
+                return 1;
+
+            // Recursão.
+            return 1 + silverman(num - silverman(silverman(num - 1)));
+        }
+
+
+        // Printa o primeiro numero na sequencia.
+        static void printSilverman(int n) {
+            // Definindo os números da sequencia.
+            for (int i = 1; i <= n; i++)
+                Console.Write(silverman(i) + " ");
+        }
+
+        // Codigo driver.
+        static void Main() {
+
+            int num = 10;
+
+            printSilverman(num);
+
+        }
+    }
+}
+
