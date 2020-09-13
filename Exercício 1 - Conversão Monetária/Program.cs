93fe7b03138df3effd1f507a837f5bf23a01aad4
João Mininél <51141319+JoaoMininelZ@users.noreply.github.com>
2020-09-13 17:46:31 -0300

    Add files via upload

---
+++ Exercício 1 - Conversão Monetária/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace ProcessoAdisra {
+    class Program {
+        static void Main() {
+            double dolar = 5.36;
+
+            Console.Write("Qual é a quantia em reais que você deseja converter para dólar americano? ");
+
+            //armazena o valor de real pra posterior conversão.
+            double.TryParse(Console.ReadLine(), out double real);
+
+            //simples conversão em uma interpolação.
+            Console.WriteLine($"A quantia de {real} convertida resulta em {real * dolar} dólares.");
+
+
+        }
+    }
+}
